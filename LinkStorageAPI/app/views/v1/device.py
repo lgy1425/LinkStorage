4c8c9892c998e72baa3a1f0be5da6ff75b164051
Medi Whale <lgy1425@gmail.com>
2021-03-08 23:35:04 +0900

    request json

--- LinkStorageAPI/app/views/v1/device.py
+++ LinkStorageAPI/app/views/v1/device.py
@@ -9,13 +9,13 @@ bp = Blueprint('v1_device', __name__, url_prefix='/v1/device')
 @bp.route('/save', methods=['POST'])
 def save():
 
-    member = Member.checkDevice(request.form["device_id"])
+    member = Member.checkDevice(request.json["device_id"])
 
     if member is None:
         m_obj = {
             "username": random_char(12),
-            "device_id": request.form["device_id"],
-            "platform": request.form["platform"]
+            "device_id": request.json["device_id"],
+            "platform": request.json["platform"]
         }
 
         member = Member(m_obj)
