ea6b399006bd93e987eaa43cf1e41cf216c935fa
Medi Whale <lgy1425@gmail.com>
2021-03-19 00:07:54 +0900

    add link view

--- LinkStorage/screens/AddLinkScreen.js
+++ LinkStorage/screens/AddLinkScreen.js
@@ -1,7 +1,15 @@
 import React, {useState} from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import {
+  View,
+  Text,
+  StyleSheet,
+  TextInput,
+  ScrollView,
+  TouchableOpacity,
+} from 'react-native';
 import DropDownPicker from 'react-native-dropdown-picker';
 import {useSelector} from 'react-redux';
+import Color from '../constants/color';
 
 const AddLinkScreen = (props) => {
   const categories = useSelector((state) => state.categories.categories);
@@ -18,20 +26,60 @@ const AddLinkScreen = (props) => {
     categoriesItems[0].value,
   );
 
+  const [URL, setURL] = useState('');
+  const [description, setDescription] = useState('');
+
+  const URLChangeHandler = (text) => {
+    setURL(text);
+  };
+
+  const descriptionChangeHandler = (text) => {
+    setDescription(text);
+  };
+
   return (
     <View style={styles.container}>
-      <View style={styles.labelWrapper}>
-        <Text style={styles.label}>Select Category</Text>
-      </View>
-      <DropDownPicker
-        items={categoriesItems}
-        defaultValue={categoriesItems[0].value}
-        containerStyle={styles.containerStyle}
-        style={styles.dropdownstyle}
-        itemStyle={styles.itemStyle}
-        dropDownStyle={styles.dropDownStyle}
-        onChangeItem={(item) => setSelectedCategory(item.value)}
-      />
+      <ScrollView style={styles.scroll}>
+        <View style={styles.labelWrapper}>
+          <Text style={styles.label}>Select Category</Text>
+        </View>
+        <DropDownPicker
+          items={categoriesItems}
+          defaultValue={categoriesItems[0].value}
+          containerStyle={styles.containerStyle}
+          style={styles.dropdownstyle}
+          itemStyle={styles.itemStyle}
+          dropDownStyle={styles.dropDownStyle}
+          onChangeItem={(item) => setSelectedCategory(item.value)}
+        />
+        <View style={styles.labelWrapper}>
+          <Text style={styles.label}>URL</Text>
+        </View>
+        <View style={styles.formControl}>
+          <TextInput
+            style={styles.textInput}
+            value={URL}
+            onChangeText={URLChangeHandler}
+            autoCapitalize="none"
+            multiline={true}
+          />
+        </View>
+        <View style={styles.labelWrapper}>
+          <Text style={styles.label}>Description</Text>
+        </View>
+        <View style={styles.formControl}>
+          <TextInput
+            style={styles.textInput}
+            value={description}
+            onChangeText={descriptionChangeHandler}
+            autoCapitalize="none"
+            multiline={true}
+          />
+        </View>
+      </ScrollView>
+      <TouchableOpacity style={styles.saveBtn}>
+        <Text style={styles.saveTxt}>Save</Text>
+      </TouchableOpacity>
     </View>
   );
 };
@@ -40,6 +88,8 @@ const styles = StyleSheet.create({
   container: {
     width: '100%',
     flexDirection: 'column',
+    justifyContent: 'flex-end',
+    flex: 1,
   },
   dropdownstyle: {
     backgroundColor: '#fafafa',
@@ -61,6 +111,34 @@ const styles = StyleSheet.create({
   },
   label: {
     fontSize: 15,
+  },
+  formControl: {
+    width: '100%',
+    paddingRight: 10,
+    paddingLeft: 10,
+  },
+  textInput: {
+    paddingHorizontal: 2,
+    paddingVertical: 5,
+    borderBottomColor: '#ccc',
+    borderBottomWidth: 1,
+  },
+  scroll: {
+    flex: 1,
+    height: '100%',
+    width: '100%',
+  },
+  saveBtn: {
+    width: '100%',
+    backgroundColor: Color.primaryColor,
+    height: 50,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  saveTxt: {
+    color: '#fff',
+    fontWeight: 'bold',
+    fontSize: 18,
   },
 });
 
