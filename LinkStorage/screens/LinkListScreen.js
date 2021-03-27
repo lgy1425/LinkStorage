c6e7aa9df20a0a484fabf88869ddaff6f1c110f2
Medi Whale <lgy1425@gmail.com>
2021-03-27 09:54:33 +0900

    search bar

--- LinkStorage/screens/LinkListScreen.js
+++ LinkStorage/screens/LinkListScreen.js
@@ -1,11 +1,18 @@
-import React, {useCallback, useEffect} from 'react';
+import React, {useCallback, useEffect, useState} from 'react';
 import {useDispatch} from 'react-redux';
-import {View, Text} from 'react-native';
+import {View, Text, StyleSheet} from 'react-native';
 import {HeaderButtons, Item} from 'react-navigation-header-buttons';
 import HeaderButton from '../components/HeaderButton';
 import * as categoryActions from '../store/action/category';
 
+import {Searchbar} from 'react-native-paper';
+
+import {Button} from 'react-native-paper';
+import Icon from 'react-native-vector-icons/Ionicons';
+
 const LinkListScreen = (props) => {
+  const [searchKey, setSearchKey] = useState('');
+
   const dispatch = useDispatch();
 
   const loadCategories = useCallback(async () => {
@@ -20,9 +27,24 @@ const LinkListScreen = (props) => {
     loadCategories().then(() => {});
   }, [dispatch, loadCategories]);
 
+  const onChangeSearch = (text) => setSearchKey(text);
+
   return (
-    <View>
-      <Text>LinkList....</Text>
+    <View style={styles.container}>
+      <View style={styles.searchBarContainer}>
+        <View style={styles.SearchBarWrapper}>
+          <Searchbar
+            placeholder="Search"
+            value={searchKey}
+            onChangeText={onChangeSearch}
+          />
+        </View>
+        <View style={styles.SearchButtonWrapper}>
+          <Button mode="contained" style={styles.searchBtn}>
+            <Icon name="md-search" size={20} />
+          </Button>
+        </View>
+      </View>
     </View>
   );
 };
@@ -54,5 +76,39 @@ LinkListScreen.navigationOptions = (navData) => {
     ),
   };
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    width: '100%',
+    height: '100%',
+  },
+  searchBarContainer: {
+    flexDirection: 'row',
+    padding: 4,
+  },
+  categorySelectWrapper: {
+    flex: 2,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  SearchBarWrapper: {
+    flex: 5,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  SearchButtonWrapper: {
+    flex: 2,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  searchBtn: {
+    width: '100%',
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
 
 export default LinkListScreen;
